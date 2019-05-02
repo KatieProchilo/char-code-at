253dc811dff2c27e8afb833471c12379db58e805
Katie Prochilo <43346846+KatieProchilo@users.noreply.github.com>
2019-05-02 14:01:24 -0700

    Added install and usage info

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
  * @param userInput The string a user has passed in to evaluate.
  */
 export const printCharCodes = (userInput, startIndex, endIndex) => {
-  console.log(`\n`);
+  console.log();
 
   for (var i = startIndex; i <= endIndex; i++) {
     console.log(`CHAR: "${userInput.charAt(i)}"\t\tCHAR CODE: ${userInput.charCodeAt(i)}`);
